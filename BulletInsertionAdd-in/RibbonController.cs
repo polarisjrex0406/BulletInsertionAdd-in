8065d308b549671758b08abfa72d45d1b49cf0d9
polarisjrex0406 <zeppelix0406@gmail.com>
2025-04-10 22:51:43 +0200

    feat: implement bullet insertion for selected text without correct format - Add icons for ribbon buttons - Implement ribbon controller - Add insert_bullet in this-add-in

---
+++ BulletInsertionAdd-in/RibbonController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new RibbonController();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace BulletInsertionAdd_in
+{
+    [ComVisible(true)]
+    public class RibbonController : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        private bool _enableBulletInsertion;
+
+        public RibbonController()
+        {
+            _enableBulletInsertion = false;
+        }
+
+        public void EnableBulletInsertion(bool isEnable)
+        {
+            _enableBulletInsertion = isEnable;
+            ribbon.Invalidate();
+        }
+
+        public void BtnBulletTriangle_OnAction(Microsoft.Office.Core.IRibbonControl control)
+        {
+            Globals.ThisAddIn.InsertBullet("Triangle");
+        }
+
+        public void BtnBulletCircle_OnAction(Microsoft.Office.Core.IRibbonControl control)
+        {
+            Globals.ThisAddIn.InsertBullet("Circle");
+        }
+
+        public void BtnBulletDash_OnAction(Microsoft.Office.Core.IRibbonControl control)
+        {
+            Globals.ThisAddIn.InsertBullet("Dash");
+        }
+
+        public bool BulletInsertion_GetEnabled(Office.IRibbonControl control)
+        {
+            return _enableBulletInsertion;
+        }
+
+        public Bitmap ImageTriangle_Icon_GetImage(IRibbonControl control)
+        {
+            return GetImage("Triangle.png");
+        }
+
+        public Bitmap ImageCircle_Icon_GetImage(IRibbonControl control)
+        {
+            return GetImage("Circle.png");
+        }
+
+        public Bitmap ImageDash_Icon_GetImage(IRibbonControl control)
+        {
+            return GetImage("Dash.png");
+        }
+
+        public Bitmap GetImage(string filename)
+        {
+            // Use the assembly to get the resource stream for the image
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"BulletInsertionAdd_in.Res.{filename}"; // Adjust namespace accordingly
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream != null)
+                {
+                    return new Bitmap(stream);
+                }
+            }
+
+            return null; // Return null if the image is not found
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("BulletInsertionAdd_in.RibbonController.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
--- BulletInsertionAdd-in/ThisAddIn.cs
+++ BulletInsertionAdd-in/ThisAddIn.cs
@@ -5,18 +5,118 @@ using System.Text;
 using System.Xml.Linq;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
 
 namespace BulletInsertionAdd_in
 {
     public partial class ThisAddIn
     {
+        private RibbonController _ribbonController;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            Application.WindowSelectionChange += OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged(PowerPoint.Selection selection)
+        {
+            bool isEnabled = CheckSelection(selection);
+            _ribbonController.EnableBulletInsertion(isEnabled);
+        }
+
+        private bool CheckSelection(PowerPoint.Selection selection)
+        {
+            // Combine checks from Steps 2 and 3
+            //return (IsMultipleTextboxes(selection) || IsMultiLineText(selection));
+            return IsMultiLineText(selection);
+        }
+
+        private bool IsMultipleTextboxes(PowerPoint.Selection selection)
+        {
+            if (selection.Type == PowerPoint.PpSelectionType.ppSelectionShapes)
+            {
+                // Check if more than one shape is selected
+                if (selection.ShapeRange.Count > 1)
+                {
+                    bool allTextboxes = true;
+                    foreach (PowerPoint.Shape shape in selection.ShapeRange)
+                    {
+                        // Verify each shape is a textbox with text
+                        if (shape.HasTextFrame != Office.MsoTriState.msoTrue ||
+                            shape.TextFrame.HasText != Office.MsoTriState.msoTrue)
+                        {
+                            allTextboxes = false;
+                            break;
+                        }
+                    }
+                    return allTextboxes; // True if multiple valid textboxes selected
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsMultiLineText(PowerPoint.Selection selection)
+        {
+            return selection.Type == PowerPoint.PpSelectionType.ppSelectionText;
+        }
+
+        public void InsertBullet(string bulletType)
+        {
+            PowerPoint.Selection selection = Application.ActiveWindow.Selection;
+            if (selection.Type != PowerPoint.PpSelectionType.ppSelectionText)
+            {
+                System.Windows.Forms.MessageBox.Show("Please select text to insert a bullet.");
+                return;
+            }
+
+            PowerPoint.TextRange textRange = selection.TextRange;
+            PowerPoint.ParagraphFormat paragraphFormat = textRange.ParagraphFormat;
+
+            paragraphFormat.Bullet.Visible = MsoTriState.msoTrue;
+            paragraphFormat.Bullet.Type = PpBulletType.ppBulletUnnumbered;
+            paragraphFormat.Bullet.UseTextColor = MsoTriState.msoFalse;
+            paragraphFormat.Bullet.UseTextFont = MsoTriState.msoFalse;
+            paragraphFormat.Bullet.Font.Color.RGB = 255;
+            paragraphFormat.Bullet.Font.Bold = MsoTriState.msoFalse;
+            paragraphFormat.Bullet.Font.Italic = MsoTriState.msoFalse;
+
+            textRange.Font.Name = "Trebuchet MS";
+            textRange.Font.Size = 8.5f;
+            textRange.Font.Bold = MsoTriState.msoFalse;
+            textRange.Font.Italic = MsoTriState.msoFalse;
+            textRange.Font.Color.RGB = 0;
+
+            switch (bulletType)
+            {
+                case "Triangle":
+                    textRange.IndentLevel = 2;
+                    paragraphFormat.Bullet.Character = 9654;
+                    paragraphFormat.Bullet.Font.Size = 5.95f;
+                    break;
+                case "Circle":
+                    textRange.IndentLevel = 3;
+                    paragraphFormat.Bullet.Character = 8226;
+                    paragraphFormat.Bullet.Font.Size = 8.5f;
+                    break;
+                case "Dash":
+                    textRange.IndentLevel = 4;
+                    paragraphFormat.Bullet.Character = 8211;
+                    paragraphFormat.Bullet.Font.Size = 8.5f;
+                    break;
+            }
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            _ribbonController = new RibbonController();
+            return _ribbonController;
+        }
+
 
         #region VSTO generated code
 
